702ee32760961b7f97aa2f9e0d550dcfb39e7ac5
aus10 <aus10.code@gmail.com>
2023-05-09 23:13:23 -0500

    Refactored away from Startup.cs
    Refactored away from Startup.cs, removed OnConfiguriung from EmployerContext and updated .gitignore

--- EFConsoleUI/Program.cs
+++ EFConsoleUI/Program.cs
@@ -1,5 +1,4 @@
-﻿
-using System.Collections.Immutable;
+﻿using System.Collections.Immutable;
 using EFMigrationEmployerLibrary.DataAccess;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
@@ -11,58 +10,37 @@ using Microsoft.Extensions.Hosting.WindowsServices;
 using Microsoft.AspNetCore.Hosting.WindowsServices;
 using Microsoft.EntityFrameworkCore.Design;
 
-
-public class Program
-{
-    public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
-
-    public static IHostBuilder CreateHostBuilder(string[] args)
-        => Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(
-                webBuilder => webBuilder.UseStartup<Startup>())
-            .UseWindowsService()
-            .UseSystemd();
-}
-
-public class Startup
-{
-    public void ConfigureServices(IServiceCollection services) => services.AddDbContext<EmployerContext>(options => options.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=EFEmployerDB;Trusted_Connection=True;"));
-
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureWebHostDefaults(webBuilder => webBuilder.ConfigureServices(services =>
     {
-    }
+        services.AddDbContext<EmployerContext>(options => options
+            .UseSqlServer(GetAppSettingsSectionValue("ConnectionStrings:Default")));
+    }))
+    .Build();
+
+AddTim();
+
+
+Console.WriteLine("lololol");
+Console.ReadLine();
+
+
+void AddTim()
+{
+    var service = host.Services.GetRequiredService<EmployerContext>();
 }
 
-// string connectionString = GetAppSettingsSectionValue("ConnectionStrings:Default");
-// //string connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=EFEmployerDB;Trusted_Connection=True;";
-//
-// AddTim();
-//
-// void AddTim()
-// {
-//     var optionsBuilder = new DbContextOptionsBuilder<EmployerContext>();
-//     optionsBuilder.UseSqlServer(connectionString);
-//
-//     using (var db = new EmployerContext(optionsBuilder))
-//     {
-//         // run CreateDatabase migration script
-//         // run ef update
-//         // build out functions
-//         // Do autoit shit
-//
-//         db.SaveChanges();
-//     }
-// }
-//
-// //Console.WriteLine($"{connectionString}");
-// Console.WriteLine("Done Processing...");
-// Console.ReadLine();
-//
-//
-// string GetAppSettingsSectionValue(string section)
-// {
-//     var config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
-//     string sectionValue = config.GetSection(section).Value;
-//
-//     return sectionValue;
-// }
+
+// run CreateDatabase migration script
+// run ef update
+// build out functions
+// Do autoit shit
+
+string GetAppSettingsSectionValue(string section)
+{
+    var config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json").Build();
+    string sectionValue = config.GetSection(section).Value;
+
+    return sectionValue;
+}
--- EFMigrationEmployerLibrary/DataAccess/EmployerContext.cs
+++ EFMigrationEmployerLibrary/DataAccess/EmployerContext.cs
@@ -1,15 +1,10 @@
 namespace EFMigrationEmployerLibrary.DataAccess;
 
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Design;
 using Models;
+
 public class EmployerContext : DbContext
 {
-    public EmployerContext(DbContextOptions<EmployerContext> options) : base(options)
-    {
-
-    }
-
     public DbSet<Person> People { get; set; }
     public DbSet<Employer> Employers { get; set; }
     public DbSet<Address> Addresses { get; set; }
@@ -17,11 +12,7 @@ public class EmployerContext : DbContext
     public DbSet<Employees> Employees { get; set; }
     public DbSet<EntityType> EntityTypes { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    public EmployerContext(DbContextOptions<EmployerContext> options) : base(options)
     {
-        if (!options.IsConfigured)
-        {
-            options.UseSqlServer(this.Database.GetConnectionString());
-        }
     }
 }
